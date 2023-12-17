f862526c232e160830dd506eab42bebd020e3615
realjioke <scarlobest@gmail.com>
2023-12-17 04:25:31 +0000

    0-uniq_users.sql

---
+++ 0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a users table. Fields: id, email, name
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);

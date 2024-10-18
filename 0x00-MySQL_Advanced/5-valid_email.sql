40c2e64553b7dabaf7e677f11c91e08367606df7
root <scarblobest@gmail.com>
2024-10-18 00:47:59 +0000

    5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,4 @@
- trigger that resets the attribute valid_email
+-- a trigger that resets the attribute valid_email
 -- only when the email has been changed.
 DELIMITER $$ ;
 CREATE TRIGGER validate  BEFORE UPDATE ON users
